f8ab626177a95ee04134d2f2d5b4c37a08e7c9ea
aprosikhin <andrewpros@mail.ru>
2017-01-02 09:11:26 -0500

    Argument parsing for index path. No connection yet between store and index.

--- poemserver/main.go
+++ poemserver/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/EastWest14/gAssert"
 	"poemXML/poemserver/handlers"
 	"poemXML/poemserver/index"
 	"poemXML/poemserver/poemstore"
@@ -9,18 +12,34 @@ import (
 
 var poemServer *server.Server
 
-const INDEX_PATH = "./index"
-
 func main() {
+	indexPath := ExtractIndexPath()
+	fmt.Printf("Index path: %s\n", indexPath)
+
+	poemServer := Setup(indexPath)
+
+	poemServer.Start()
+}
+
+func ExtractIndexPath() string {
+	flag.Parse()
+	numArgs := flag.NArg()
+	gAssert.AssertHard(numArgs > 0, "Number of command line arguments is 0 - can't extract index path.")
+	fmt.Printf("Number of arguments: %d\n", flag.NArg())
+
+	return flag.Arg(0)
+}
+
+func Setup(indexPath string) *server.Server {
 	poemServer = server.NewServer()
 
 	handlers := handlers.NewHandlersInstance()
 	poemServer.SetHandlers(handlers)
 
-	theIndex := index.New(INDEX_PATH)
+	theIndex := index.New(indexPath)
 
 	poemStore := poemstore.NewStore(theIndex)
 	handlers.SetPoemStore(poemStore)
 
-	poemServer.Start()
+	return poemServer
 }
