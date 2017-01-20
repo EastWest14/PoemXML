eeb1b8f6ad83425d7803d8b5095d5cbf03cb73fb
aprosikhin <andrewpros@mail.ru>
2017-01-19 19:15:33 -0500

    Test and launch cleaing up the running process.

--- test_and_launch.sh
+++ test_and_launch.sh
@@ -3,9 +3,14 @@ go test ./...
 [ $? -eq 0 ] || exit $?;
 echo Unit Tests Pass!
 
-go run poemserver/main.go 'poemserver/index/test_indices/regression_test_index.xml' &
+echo Building the server.
+go build poemserver/main.go
+[ $? -eq 0 ] || exit $?;
+
+echo Launching the server
+./main 'poemserver/index/test_indices/regression_test_index.xml' &
 ID=$!;
-#[ $? -eq 0 ] || echo Failed to launch the server!; exit $?;
+[ $? -eq 0 ] || exit $?;
 
 cd regression/Java_Regression/src
 sleep 3
