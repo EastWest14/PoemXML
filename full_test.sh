5b8accb470f3b0c88952d9838b37df000feeb74f
Andrew Prosikhin <andrewpros@mail.ru>
2017-02-16 23:30:42 -0500

    Index is now being copied and then cleaned up during the full test.

--- full_test.sh
+++ full_test.sh
@@ -20,9 +20,29 @@ echo '===================================='
 echo ''
 
 echo '===================================='
+echo 'Preparing the index directory copy:'
+set -e
+original_regression_index_path="./poemserver/index/test_indices/regression_test_index/*"
+
+namebase="index_workcopy_"
+datepart=$(date +%s%N)
+new_dir_name="$namebase$datepart"
+
+temp_index_dir=$(mktemp -d $new_dir_name)
+cp -R $original_regression_index_path $temp_index_dir
+set +e
+echo 'Index Directory Copy Created'
+echo '===================================='
+echo ''
+
+echo '===================================='
 echo 'Launching the Server:'
-./main 'poemserver/index/test_indices/regression_test_index.xml' &
+set -e
+index_name="regression_test_index.xml"
+path_to_index_xml="$temp_index_dir/$index_name"
+./main $path_to_index_xml &
 ID=$!;
+set +e
 sleep 2
 echo 'Server Launched'
 echo '===================================='
@@ -33,7 +53,6 @@ echo 'Launching Regression suit:'
 cd regression/Java_Regression/src
 sleep 1
 java Tester.Tester
-[ $? -eq 0 ] || exit $?;
 cd -
 echo 'Regression Suit Passes'
 echo '===================================='
@@ -41,9 +60,23 @@ echo ''
 
 echo '===================================='
 echo 'Shutting down the server:'
-pkill -P $$;
-[ $? -eq 0 ] || exit $?;
+set -e
+echo 'Process ID: ' $ID
+kill $ID
+set +e
 echo 'Server Shut Down'
+echo '===================================='
+echo ''
+
+echo '===================================='
+echo 'Removing index regression copy:'
+set -e
+cd $temp_index_dir
+rm *
+cd -
+rmdir $temp_index_dir
+set +e
+echo '`index regression copy removed'
 echo '===================================='
 echo ''
 
