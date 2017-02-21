70131ca939b074b6448cd0161926843b3d3dadcd
Andrew Prosikhin <andrewpros@mail.ru>
2017-02-20 20:58:42 -0500

    Bash connection to DB.

--- create_regression_db.sh
+++ create_regression_db.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+if ! [ $DB_USER ]
+then
+	echo 'DB_USER environment variable not set will not be able to connect to DB.'
+	echo 'Set DB_USER environment variable and preferable put it in a script, for example set_env_var.sh.'
+	exit 1
+fi
+
+if ! [ $DB_PASSWORD ]
+then
+        echo 'DB_PASSWORD environment variable not set will not be able to connect to DB.'
+        echo 'Set DB_PASSWORD environment variable and preferable put it in a script, for example set_env_var.sh.'
+        exit 1
+fi
+
 if [ $OSTYPE = 'linux-gnu' ]
 then
-	echo 'Linux'
+	echo 'Running on Linux OS. Assuming EC2 environment.'
+	if ! [ $DB_HOST ]
+	then
+        	echo 'DB_HOST environment variable not set will not be able to connect to DB.'
+        	echo 'Set DB_HOST environment variable and preferable put it in a script, for example set_env_var.sh.'
+        	exit 1
+	fi
+	mysql -h$DB_HOST -u$DB_USER -p$DB_PASSWORD -e "show databases;"
 else 
-	echo 'Not-Linux'
+	echo 'Running on a non-Linux OS. Assuming local environment.'
+	mysql -u$DB_USER -p$DB_PASSWORD -e "show databases;"
 fi
